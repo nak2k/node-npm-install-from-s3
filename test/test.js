e62db87535b35de4c15aca9af485c64c04ee829d
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-03-15 12:04:15 +0900

    Test when prepublishOnly

--- test/test.js
+++ test/test.js
@@ -3,16 +3,12 @@ const { installFromS3 } = require('..');
 
 test('test', t => {
   t.plan(1);
-  if (process.env.S3_URL) {
-    installFromS3([process.env.S3_URL], {}, err => {
-      if (err) {
-        return t.end(err);
-      }
 
-      t.ok(true);
-    });
-  } else {
-    // would be better to have a permanent S3 URL for testing...
+  installFromS3([process.env.S3_URL], {}, err => {
+    if (err) {
+      return t.end(err);
+    }
+
     t.ok(true);
-  }
+  });
 });
